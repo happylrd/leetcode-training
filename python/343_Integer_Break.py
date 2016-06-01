bc887cae80409b587175b71bbdc2a3283d7765dc
Diego Li <282108074@qq.com>
2016-06-01 21:51:48 +0800

    2016/6/1 343

---
+++ python/343_Integer_Break.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def integerBreak(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        div = n / 3
+        if div <= 1:
+            return (n / 2) * (n / 2 + n % 2)
+
+        mod = n % 3
+        if mod == 0:
+            return 3 ** div
+        elif mod == 1:
+            return 3 ** (div - 1) * 4
+        elif mod == 2:
+            return 3 ** div * 2
+
+
+def test():
+    s = Solution()
+    print s.integerBreak(2)
+
+
+if __name__ == '__main__':
+    test()
