518ac3e4998b2673795b11a0fb09504c2ddd5844
Diego Li <282108074@qq.com>
2016-06-03 19:57:16 +0800

    2016/6/3 231

--- python/231_Power_of_Two.py
+++ python/231_Power_of_Two.py
@@ -6,3 +6,12 @@ class Solution(object):
         """
         # power of two only have one 1 in binary system
         return n > 0 and n & (n - 1) == 0
+
+
+def test():
+    s = Solution()
+    print s.isPowerOfTwo(8);
+
+
+if __name__ == '__main__':
+    test()
