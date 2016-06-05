3727767f07c835c924d85888ecffbd9d7622164f
Diego Li <282108074@qq.com>
2016-06-05 18:44:31 +0800

    2016/6/5 136

---
+++ C#/136_Single_Number.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LeetCode
+{
+    public class Solution
+    {
+        public int SingleNumber(int[] nums)
+        {
+            int ans = 0;         
+            foreach (int num in nums)
+            {
+                ans ^= num;
+            }
+            return ans;
+        }
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            Console.WriteLine(s.SingleNumber(new int[] {
+                0, 0, 1, 1, 2, 3, 3, 4, 4, 5, 5 }));
+        }
+    }
+}
