aa3601652d565ce9b75ec5015371044d1fbd262f
Diego Li <282108074@qq.com>
2016-06-07 16:49:47 +0800

    2016/6/7 231

---
+++ C#/231_Power_of_Two.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LeetCode
+{
+    public class Solution
+    {
+        public bool IsPowerOfTwo(int n)
+        {
+            return n > 0 && (n & (n - 1)) == 0;
+        }
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            Console.Write(s.IsPowerOfTwo(8));
+        }
+    }
+}
