832b9844c7c5f4c40c02e5889cf33078c6999b78
Diego Li <282108074@qq.com>
2016-06-07 16:26:46 +0800

    2016/6/7 171

---
+++ C#/171_Excel_Sheet_Column_Number.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LeetCode
+{
+    public class Solution
+    {
+        public int TitleToNumber(string s)
+        {
+            int ans = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                ans = ans * 26 + s[i] - 'A' + 1;
+            }
+            return ans;
+        }
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            Console.Write(s.TitleToNumber("AB"));
+        }
+    }
+}
