072e542a1cdf518573cb2ee1ea53ad45febdcdf6
Diego Li <282108074@qq.com>
2016-06-08 22:11:01 +0800

    2016/6/8 338

---
+++ C#/338_Counting_Bits.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+
+namespace LeetCode
+{
+    public class Solution
+    {
+        public int[] CountBits(int num)
+        {
+            ArrayList ans = new ArrayList();
+            ans.Add(0);
+            for (int i = 1; i <= num; i++)
+            {
+                ans.Add((int)ans[(i >> 1)] + (i & 1));
+            }
+            return (int[])ans.ToArray(typeof(int));
+        }
+
+        //Testcase
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            Console.Write("Input the int: ");
+            int[] temp = s.CountBits(
+                Convert.ToInt32(Console.ReadLine()));
+            Console.Write("[");
+            for (int i = 0; i < temp.Length; i++)
+            {
+                Console.Write(temp[i]);
+                if (i == temp.Length - 1)
+                    break;
+                Console.Write(",");
+            }
+            Console.Write("]");
+        }
+    }
+}
