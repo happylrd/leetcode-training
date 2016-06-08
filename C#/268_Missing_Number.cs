72eebd97a15541e15fc69ad381b1c45e7bcddac0
Diego Li <282108074@qq.com>
2016-06-08 21:02:31 +0800

    2016/6/8 268

---
+++ C#/268_Missing_Number.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace LeetCode
+{
+    public class Solution
+    {
+        public int MissingNumber(int[] nums)
+        {
+            int n = nums.Length;
+            int sum = 0;
+            foreach(int num in nums)
+            {
+                sum += num;
+            }
+            return n * (n + 1) / 2 - sum;
+        }
+
+        public int MissingNumber2(int[] nums)
+        {
+            int a = 1, b = 1;
+            foreach(int num in nums)
+            {
+                a ^= num;
+            }
+            for (int i = 0; i < nums.Length + 1; i++)
+            {
+                b ^= i;
+            }
+            return a ^ b;
+        }
+
+        //Testcase
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            Console.Write(s.MissingNumber(new int[] { 0, 1, 3 }));
+        }
+    }
+}
