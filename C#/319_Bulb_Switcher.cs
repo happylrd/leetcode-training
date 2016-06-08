a425475435be51d3940b141928e547a79af4f3ea
Diego Li <282108074@qq.com>
2016-06-08 21:18:32 +0800

    2016/6/8 319

---
+++ C#/319_Bulb_Switcher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LeetCode
+{
+    public class Solution
+    {
+        public int BulbSwitch(int n)
+        {
+            return (int)Math.Sqrt(n);
+        }
+
+        //Testcase
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            Console.Write(s.BulbSwitch(5));
+            // final state: 1 0 0 1 0 , n = 2
+        }
+    }
+}
