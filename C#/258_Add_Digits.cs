d58c6015153021e0a9bfdae52b34bccdae721b9a
Diego Li <282108074@qq.com>
2016-06-08 20:40:44 +0800

    2016/6/8 258

---
+++ C#/258_Add_Digits.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LeetCode
+{
+    public class Solution
+    {
+        public int AddDigits(int num)
+        {
+            while (num > 9)
+            {
+                int temp_sum = 0;
+                while (num != 0)
+                {
+                    temp_sum += num % 10;
+                    num /= 10;
+                }
+                num = temp_sum;
+            }
+            return num;
+        }
+
+        public int AddDigits2(int num)
+        {
+            if (num == 0)
+                return 0;
+            return (num - 1) % 9 + 1;
+        }
+
+        //Testcase
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            Console.Write(s.AddDigits(38));
+        }
+    }
+}
