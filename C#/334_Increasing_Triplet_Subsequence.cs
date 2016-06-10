35f1b899c92c012d4d9f7b807c8830c8dbd18080
Diego Li <282108074@qq.com>
2016-06-10 17:40:18 +0800

    2016/6/10 334

---
+++ C#/334_Increasing_Triplet_Subsequence.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LeetCode
+{
+    public class Solution
+    {
+        public bool IncreasingTriplet(int[] nums)
+        {
+            int a = int.MaxValue;
+            int b = int.MaxValue;
+            foreach (int elem in nums)
+            {
+                if (a >= elem)
+                    a = elem;
+                else if (b >= elem)
+                    b = elem;
+                else
+                    return true;
+            }
+            return false;
+        }
+
+        //Testcase
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            Console.Write(s.IncreasingTriplet(new int[] { 0, -1, 1, -3, 3 }));
+        }
+    }
+}
