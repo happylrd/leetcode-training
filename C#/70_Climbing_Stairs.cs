8b6321c7933c5b76944f88c0bd954abf2db4bb56
Diego Li <282108074@qq.com>
2016-07-26 17:00:51 +0800

    2016/7/26 70

---
+++ C#/70_Climbing_Stairs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LeetCode
+{
+    public class Solution
+    {
+        public int ClimbStairs(int n)
+        {
+            int a = 1;
+            int b = 1;
+            for (int i = 2; i < n + 1; i++)
+            {
+                int temp = a + b;
+                a = b;
+                b = temp;
+            }
+            return b;
+        }
+
+        //Testcase
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            int stair_num = new Random().Next(1, 21);
+            Console.Write("Number of layers: " + stair_num +
+                "\tNumber of steps: " + s.ClimbStairs(stair_num));           
+        }
+    }
+}
