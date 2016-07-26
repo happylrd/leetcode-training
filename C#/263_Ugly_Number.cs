2030c322410fb40d77bb895bd7eabb938f9b1d4c
Diego Li <282108074@qq.com>
2016-07-26 16:59:49 +0800

    2016/7/26 263

---
+++ C#/263_Ugly_Number.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LeetCode
+{
+    public class Solution
+    {
+        public bool IsUgly(int num)
+        {
+            if (num <= 0)
+                return false;
+            int[] arr_factors = { 2, 3, 5 };
+            foreach (int elem in arr_factors)
+            {
+                while (num % elem == 0)
+                {
+                    num /= elem;
+                }
+            }
+            return num == 1;
+        }
+
+        //Testcase
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            int rand_num = new Random().Next(0, 11);
+            if (s.IsUgly(rand_num))
+                Console.Write(rand_num + " is a ugly number.");
+            else
+                Console.Write(rand_num + " isn't a ugly number.");
+        }
+    }
+}
