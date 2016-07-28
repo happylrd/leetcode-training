8310102bd1b13e63cb6efbdf25e0e2691edc0633
Diego Li <282108074@qq.com>
2016-07-28 13:49:01 +0800

    2016/7/28 292

---
+++ C#/292_Nim_Game.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LeetCode
+{
+    public class Solution
+    {
+        public bool CanWinNim(int n)
+        {
+            return n % 4 != 0;
+        }
+
+        //Testcase
+        static void Main(string[] args)
+        {
+            Solution sol = new Solution();
+            int stone_num = new Random().Next(1, 1001);
+
+            if (sol.CanWinNim(stone_num))
+                Console.WriteLine("When the number of stones are " + stone_num + ", first win!");
+            else
+                Console.WriteLine("When the number of stones are " + stone_num + ", second win!");
+
+        }
+    }
+}
